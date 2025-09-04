dfeccdf9b54f17a2ab5ba340058e446c291b7c3b
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-09-04 11:24:41 +0000

    test: collect_imgfile_basenames

--- src/utils/collect_imgfile_basenames.rs
+++ src/utils/collect_imgfile_basenames.rs
@@ -24,3 +24,118 @@ pub fn collect_imgfile_basenames(
     }
     basenames
 }
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use super::*;
+
+    #[test]
+    fn test_collect_only_image_files() {
+        let temp_dir = tempfile::tempdir().unwrap();
+        let temp_dir_path = temp_dir.path().to_path_buf();
+        fs::write(temp_dir_path.join("image1.jpg"), b"").unwrap();
+        fs::write(temp_dir_path.join("image2.PNG"), b"").unwrap();
+        fs::write(temp_dir_path.join("not_image.txt"), b"").unwrap();
+
+        let imgdir_paths = vec![temp_dir_path];
+        let result = collect_imgfile_basenames(&imgdir_paths);
+
+        assert_eq!(result.len(), 2);
+        assert!(result.contains("image1.jpg"));
+        assert!(result.contains("image2.PNG"));
+        assert!(!result.contains("not_an_image.txt"));
+    }
+
+    #[test]
+    fn test_dont_collect_deep_images() {
+        let temp_dir = tempfile::tempdir().unwrap();
+        let temp_dir_path = temp_dir.path().to_path_buf();
+        fs::create_dir(temp_dir_path.join("subdir")).unwrap();
+        fs::write(temp_dir_path.join("subdir").join("image.jpg"), b"").unwrap();
+
+        let imgdir_paths = vec![temp_dir_path];
+        let result = collect_imgfile_basenames(&imgdir_paths);
+
+        assert!(result.is_empty());
+    }
+
+    #[test]
+    fn test_two_dirs_different_stems_same_ext() {
+        let temp_dir1 = tempfile::tempdir().unwrap();
+        let temp_dir_path1 = temp_dir1.path().to_path_buf();
+        fs::write(temp_dir_path1.join("image1.jpg"), b"").unwrap();
+
+        let temp_dir2 = tempfile::tempdir().unwrap();
+        let temp_dir_path2 = temp_dir2.path().to_path_buf();
+        fs::write(temp_dir_path2.join("image2.jpg"), b"").unwrap();
+
+        let imgdir_paths = vec![temp_dir_path1, temp_dir_path2];
+        let result = collect_imgfile_basenames(&imgdir_paths);
+
+        assert_eq!(result.len(), 2);
+        assert!(result.contains("image1.jpg"));
+        assert!(result.contains("image2.jpg"));
+    }
+
+    #[test]
+    fn test_two_dirs_same_stem_different_ext() {
+        let temp_dir1 = tempfile::tempdir().unwrap();
+        let temp_dir_path1 = temp_dir1.path().to_path_buf();
+        fs::write(temp_dir_path1.join("image1.jpg"), b"").unwrap();
+
+        let temp_dir2 = tempfile::tempdir().unwrap();
+        let temp_dir_path2 = temp_dir2.path().to_path_buf();
+        fs::write(temp_dir_path2.join("image1.png"), b"").unwrap();
+
+        let imgdir_paths = vec![temp_dir_path1, temp_dir_path2];
+        let result = collect_imgfile_basenames(&imgdir_paths);
+
+        assert_eq!(result.len(), 2);
+        assert!(result.contains("image1.jpg"));
+        assert!(result.contains("image1.png"));
+    }
+
+    #[test]
+    fn test_two_dirs_same_stem_same_ext() {
+        let temp_dir1 = tempfile::tempdir().unwrap();
+        let temp_dir_path1 = temp_dir1.path().to_path_buf();
+        fs::write(temp_dir_path1.join("image1.jpg"), b"").unwrap();
+
+        let temp_dir2 = tempfile::tempdir().unwrap();
+        let temp_dir_path2 = temp_dir2.path().to_path_buf();
+        fs::write(temp_dir_path2.join("image1.jpg"), b"").unwrap();
+
+        let imgdir_paths = vec![temp_dir_path1, temp_dir_path2];
+        let result = collect_imgfile_basenames(&imgdir_paths);
+
+        assert_eq!(result.len(), 1);
+        assert!(result.contains("image1.jpg"));
+        assert!(result.contains("image1.jpg"));
+    }
+
+    #[test]
+    fn test_two_dirs_no_images() {
+        let temp_dir = tempfile::tempdir().unwrap();
+        let temp_dir_path = temp_dir.path().to_path_buf();
+        fs::write(temp_dir_path.join("not_an_image.txt"), b"").unwrap();
+
+        let imgdir_paths = vec![temp_dir_path];
+        let result = collect_imgfile_basenames(&imgdir_paths);
+
+        assert!(result.is_empty());
+    }
+
+    #[test]
+    fn test_dont_collect_image_extra_extension() {
+        let temp_dir = tempfile::tempdir().unwrap();
+        let temp_dir_path = temp_dir.path().to_path_buf();
+        fs::write(temp_dir_path.join("image.jpg.extra"), b"").unwrap();
+
+        let imgdir_paths = vec![temp_dir_path];
+        let result = collect_imgfile_basenames(&imgdir_paths);
+
+        assert!(result.is_empty());
+    }
+}
