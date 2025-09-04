066140fad5fcde93849aa696aee642bb05ae955e
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-09-04 15:40:43 +0000

    feat: add new_split to Pane

--- src/panes.rs
+++ src/panes.rs
@@ -16,6 +16,15 @@ impl Pane {
     pub fn new_leaf() -> Self {
         Pane::Leaf { image_id: 0 }
     }
+
+    pub fn new_split(direction: layout::Direction) -> Pane {
+        Pane::Split {
+            direction,
+            pct: 50,
+            first: Box::new(Self::new_leaf()),
+            second: Box::new(Self::new_leaf()),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -27,5 +36,18 @@ mod tests {
         let tree = Pane::new_leaf();
 
         assert!(matches!(tree, Pane::Leaf { .. }));
+    }
+
+    #[test]
+    fn test_new_split() {
+        let tree = Pane::new_split(layout::Direction::Horizontal);
+
+        assert!(matches!(
+            tree,
+            Pane::Split {
+                direction: layout::Direction::Horizontal,
+                ..
+            }
+        ));
     }
 }
