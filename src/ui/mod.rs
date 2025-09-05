de5786746f481bf7d69f3d8e0e53c35fbc0fdbe6
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-09-05 14:28:09 +0000

    refactor: rename

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,7 +1,7 @@
 mod image_layout;
-mod tab_ui;
+mod navbar_ui;
 mod table_ui;
 
 pub use image_layout::ImageLayout;
-pub use tab_ui::NavBarUI;
+pub use navbar_ui::NavBarUI;
 pub use table_ui::TableUI;
