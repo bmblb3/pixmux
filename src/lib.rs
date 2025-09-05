d0be5b82edb16fc87912afc5d66fb9698ab7cdf2
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-09-05 13:34:26 +0000

    refactor!: rename lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 pub mod adjust_index;
-pub mod collect_imgfile_basenames;
+pub mod collect_imagefile_basenames;
 pub mod panes;
 pub mod parse_csv;
 
 pub use adjust_index::{AdjustDirection, cycle_index, step_index};
-pub use collect_imgfile_basenames::collect_imgfile_basenames;
+pub use collect_imagefile_basenames::collect_imgfile_basenames;
 pub use panes::Pane;
 pub use parse_csv::parse_csv;
