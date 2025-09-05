f0e5701f4686aaa05b138faa00e5e0dcd006b855
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-09-05 13:24:47 +0000

    refactor: simplify image pane rendering logic

--- src/ui/image_layout.rs
+++ src/ui/image_layout.rs
@@ -4,9 +4,8 @@ use pixmux::Pane;
 use ratatui::Frame;
 use ratatui::layout::{Constraint, Layout, Rect};
 use ratatui::style::Color;
-use ratatui::widgets::{Block, BorderType};
-use ratatui_image::StatefulImage;
-use ratatui_image::picker::Picker;
+use ratatui::widgets::Block;
+use ratatui_image::{StatefulImage, picker};
 
 use crate::app::App;
 
@@ -14,35 +13,28 @@ use crate::app::App;
 pub struct ImageLayout;
 
 impl ImageLayout {
-    fn render_imgpane(frame: &mut Frame, area: Rect, pane: &Pane, app: &App) {
+    fn render_pane(pane: &Pane, frame: &mut Frame, area: Rect, app: &App, picker: &picker::Picker) {
         match pane {
             Pane::Leaf { image_id } => {
-                let block = Block::bordered().border_type(BorderType::QuadrantInside);
+                let block = Block::bordered();
+
+                let current_pane = app.pane_tree.get_node_at(&app.current_pane_path).unwrap();
+                if ptr::eq(current_pane, pane) {
+                    frame.render_widget(block.clone().style(Color::LightYellow), area);
+                } else {
+                    frame.render_widget(block.clone(), area);
+                }
+
                 let imagefile = app.get_fullimgpath(image_id, &app.current_row_index);
-                match imagefile {
-                    Some(f) => {
-                        let picker = Picker::from_query_stdio().unwrap();
-                        let image_source = image::ImageReader::open(f).unwrap().decode().unwrap();
-                        let mut image = picker.new_resize_protocol(image_source);
+                if let Some(f) = imagefile {
+                    let image_source = image::ImageReader::open(f).unwrap().decode().unwrap();
+                    let mut image = picker.new_resize_protocol(image_source);
 
-                        let current_pane =
-                            app.pane_tree.get_node_at(&app.current_pane_path).unwrap();
-
-                        if ptr::eq(current_pane, pane) {
-                            frame.render_widget(block.clone().style(Color::LightYellow), area);
-                        } else {
-                            frame.render_widget(block.clone(), area);
-                        }
-
-                        frame.render_stateful_widget(
-                            StatefulImage::default(),
-                            block.inner(area),
-                            &mut image,
-                        );
-                    }
-                    _ => {
-                        frame.render_widget(block, area);
-                    }
+                    frame.render_stateful_widget(
+                        StatefulImage::default(),
+                        block.inner(area),
+                        &mut image,
+                    );
                 }
             }
             Pane::Split {
@@ -61,13 +53,14 @@ impl ImageLayout {
                     .direction(*direction)
                     .constraints(constraints)
                     .split(area);
-                Self::render_imgpane(frame, chunks[0], first, app);
-                Self::render_imgpane(frame, chunks[1], second, app);
+                Self::render_pane(first, frame, chunks[0], app, picker);
+                Self::render_pane(second, frame, chunks[1], app, picker);
             }
         }
     }
 
     pub fn render(frame: &mut Frame, area: Rect, app: &App) {
-        Self::render_imgpane(frame, area, &app.pane_tree, app);
+        let picker = picker::Picker::from_query_stdio().unwrap();
+        Self::render_pane(&app.pane_tree, frame, area, app, &picker);
     }
 }
