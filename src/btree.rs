2876d89c6d63815a5d180807416b4b087b99770b
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-09-06 21:04:30 +0000

    feat(Btree): leaf with data

--- src/btree.rs
+++ src/btree.rs
@@ -1,7 +1,7 @@
 pub enum BTreeNode<L = ()> {
     Leaf(L),
 }
-impl BTreeNode {
+impl<L> BTreeNode<L> {
     pub fn get_paths(&self) -> Vec<Vec<bool>> {
         let paths = vec![vec![]];
         paths
@@ -15,6 +15,15 @@ mod tests {
     #[test]
     fn test_btree_with_leaf_at_root_returns_computed_paths() {
         let tree = BTreeNode::Leaf(());
+
+        let paths = tree.get_paths();
+
+        assert_eq!(paths, [[]]);
+    }
+
+    #[test]
+    fn test_btree_with_leaf_at_root_with_data_returns_computed_paths() {
+        let tree = BTreeNode::Leaf(42);
 
         let paths = tree.get_paths();
 
