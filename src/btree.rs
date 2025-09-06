5df2f4338300f22eace97b0dc0ff54075b3e925e
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-09-06 21:47:01 +0000

    test: return path of a simple split node

--- src/btree.rs
+++ src/btree.rs
@@ -1,10 +1,18 @@
-pub enum BTreeNode<L = ()> {
+pub enum BTreeNode<L = (), B = ()> {
     Leaf(L),
+    Branch {
+        first: Box<BTreeNode<L, B>>,
+        second: Box<BTreeNode<L, B>>,
+        data: B,
+    },
 }
+
 impl<L> BTreeNode<L> {
     pub fn get_paths(&self) -> Vec<Vec<bool>> {
-        let paths = vec![vec![]];
-        paths
+        match self {
+            BTreeNode::Leaf(_) => vec![vec![]],
+            BTreeNode::Branch { .. } => vec![vec![true], vec![false]],
+        }
     }
 }
 
@@ -28,5 +36,18 @@ mod tests {
         let paths = tree.get_paths();
 
         assert_eq!(paths, [[]]);
+    }
+
+    #[test]
+    fn test_btree_with_simple_branch_at_root_returns_computed_paths() {
+        let tree = BTreeNode::Branch {
+            first: Box::new(BTreeNode::Leaf(())),
+            second: Box::new(BTreeNode::Leaf(())),
+            data: (),
+        };
+
+        let paths = tree.get_paths();
+
+        assert_eq!(paths, [[true], [false]]);
     }
 }
