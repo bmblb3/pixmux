cb7c3df5077d0590a3eb0c6645509fd2e7872201
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-09-06 22:18:18 +0000

    test: first heavy tree

--- src/btree.rs
+++ src/btree.rs
@@ -11,7 +11,29 @@ impl<L, B> BTreeNode<L, B> {
     pub fn get_paths(&self) -> Vec<Vec<bool>> {
         match self {
             BTreeNode::Leaf(_) => vec![vec![]],
-            BTreeNode::Branch { .. } => vec![vec![true], vec![false]],
+            BTreeNode::Branch { .. } => {
+                let mut all_paths = Vec::new();
+                let mut current_path = Vec::new();
+                Self::get_paths_impl(self, &mut current_path, &mut all_paths);
+                all_paths
+            }
+        }
+    }
+
+    fn get_paths_impl(node: &Self, current_path: &mut Vec<bool>, all_paths: &mut Vec<Vec<bool>>) {
+        match node {
+            Self::Leaf(_) => {
+                all_paths.push(current_path.to_vec());
+            }
+            Self::Branch { first, second, .. } => {
+                current_path.push(true);
+                Self::get_paths_impl(first, current_path, all_paths);
+                current_path.pop();
+
+                current_path.push(false);
+                Self::get_paths_impl(second, current_path, all_paths);
+                current_path.pop();
+            }
         }
     }
 }
@@ -66,5 +88,31 @@ mod tests {
         let paths = tree.get_paths();
 
         assert_eq!(paths, [[true], [false]]);
+    }
+
+    impl TestBTree {
+        fn new_leaf() -> Self {
+            BTreeNode::Leaf(())
+        }
+
+        fn new_branch(first: BTreeNode<(), ()>, second: BTreeNode<(), ()>) -> Self {
+            BTreeNode::Branch {
+                first: Box::new(first),
+                second: Box::new(second),
+                data: (),
+            }
+        }
+    }
+
+    #[test]
+    fn test_btree_with_first_heavy_branch_returns_computed_paths() {
+        let tree = TestBTree::new_branch(
+            TestBTree::new_branch(TestBTree::new_leaf(), TestBTree::new_leaf()),
+            TestBTree::new_leaf(),
+        );
+
+        let paths = tree.get_paths();
+
+        assert_eq!(paths, [vec![true, true], vec![true, false], vec![false]]);
     }
 }
